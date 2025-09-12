5614b7e0699fc9adcbedf07b3d1825a902b40565
matheusgesser <matheusmvg@hotmail.com>
2025-09-12 07:11:39 -0300

    feat: add playlist privacy status title translation

--- src/components/PlaylistList.tsx
+++ src/components/PlaylistList.tsx
@@ -1,12 +1,14 @@
 import { Link } from '@/i18n/routing';
 import { Playlist } from '@/types/Playlist';
 import { Lock, LinkSimple, Globe } from '@phosphor-icons/react/dist/ssr';
+import { useTranslations } from 'next-intl';
 
 type Props = {
     label: string,
     playlists: Playlist.Model[],
 };
 
+/** @factory */
 const makePrivacyIcon = (privacyStatus: Playlist.Model['privacyStatus']) => {
     if (privacyStatus === 'private')
         return <Lock size={14} className="text-neutral-500" />;
@@ -21,6 +23,22 @@ const makePrivacyIcon = (privacyStatus: Playlist.Model['privacyStatus']) => {
 };
 
 export function PlaylistList({ label, playlists }: Props) {
+    const translate = useTranslations();
+
+    /** @factory */
+    const makePrivacyLabel = (privacyStatus: Playlist.Model['privacyStatus']) => {
+        if (privacyStatus === 'private')
+            return translate('private');
+
+        if (privacyStatus === 'unlisted')
+            return translate('unlisted');
+
+        if (privacyStatus === 'public')
+            return translate('public');
+
+        return undefined;
+    };
+
     return (
         <div className="w-full self-start flex flex-col gap-2">
             <span>{label}</span>
@@ -30,7 +48,9 @@ export function PlaylistList({ label, playlists }: Props) {
                     <div className="flex items-center gap-1">
                         <span className="text-lg">{playlist.title ?? playlist.id}</span>
 
-                        {makePrivacyIcon(playlist.privacyStatus)}
+                        <div title={makePrivacyLabel(playlist.privacyStatus)}>
+                            {makePrivacyIcon(playlist.privacyStatus)}
+                        </div>
                     </div>
 
                     <span className="px-2 py-0.5 ml-auto">{playlist.videos.length}</span>
